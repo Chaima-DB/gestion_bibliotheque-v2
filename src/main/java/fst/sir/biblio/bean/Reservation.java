403b7d445e2c642ecdad0a16be6b24f7b155bf59
aicha <ai.elabdellaoui@gmail.com>
2020-02-22 17:41:30 +0100

    reservation

---
+++ src/main/java/fst/sir/biblio/bean/Reservation.java
@@ -0,0 +1,19 @@
+package fst.sir.biblio.bean;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
+
+
+@Entity
+
+public class Reservation  implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+}
